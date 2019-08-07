c9908d4024d81d0b1ea0e0b6c9dc1d0ce3b1f417
ifcquery <fabian.gerold@gmail.com>
2019-08-07 12:07:01 -0500

    fixed HEADER and ENDSEC

--- IfcPlusPlus/src/ifcpp/writer/WriterSTEP.cpp
+++ IfcPlusPlus/src/ifcpp/writer/WriterSTEP.cpp
@@ -39,9 +39,7 @@ void WriterSTEP::writeModelToStream( std::stringstream& stream, shared_ptr<Build
 	const std::wstring& file_header_wstr = model->getFileHeader();
 	std::string file_header_str = ws2s( file_header_wstr );
 	stream << "ISO-10303-21;\n";
-	stream << "HEADER;\n";
 	stream << file_header_str.c_str();
-	stream << "ENDSEC;\n";
 	stream << "DATA;\n";
 	stream << std::setprecision( 15 );
 	stream << std::setiosflags( std::ios::showpoint );
